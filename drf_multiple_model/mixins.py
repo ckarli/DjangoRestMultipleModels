0c3b5b80059ed037b79926c560b2548810da4d51
ckarli <cihat.karli@yapitasi.com>
2024-07-01 14:24:31 +0300

    Added custom filter_queryset method to safely override viewset filterset_class

--- drf_multiple_model/mixins.py
+++ drf_multiple_model/mixins.py
@@ -1,4 +1,5 @@
 import warnings
+from copy import deepcopy
 
 from django.core.exceptions import ValidationError
 from django.db.models.query import QuerySet
@@ -9,20 +10,20 @@ class BaseMultipleModelMixin(object):
     """
     Base class that holds functions need for all MultipleModelMixins/Views
     """
+
     querylist = None
 
     # Keys required for every item in a querylist
-    required_keys = ['queryset', 'serializer_class']
+    required_keys = ["queryset", "serializer_class"]
 
     # default pagination state. Gets overridden if pagination is active
     is_paginated = False
     default_filterset_class = None
+
     def get_querylist(self):
         assert self.querylist is not None, (
-            '{} should either include a `querylist` attribute, '
-            'or override the `get_querylist()` method.'.format(
-                self.__class__.__name__
-            )
+            "{} should either include a `querylist` attribute, "
+            "or override the `get_querylist()` method.".format(self.__class__.__name__)
         )
 
         return self.querylist
@@ -36,8 +37,8 @@ class BaseMultipleModelMixin(object):
         for key in self.required_keys:
             if key not in query_data:
                 raise ValidationError(
-                    'All items in the {} querylist attribute should contain a '
-                    '`{}` key'.format(self.__class__.__name__, key)
+                    "All items in the {} querylist attribute should contain a "
+                    "`{}` key".format(self.__class__.__name__, key)
                 )
 
     def load_queryset(self, query_data, request, *args, **kwargs):
@@ -46,26 +47,17 @@ class BaseMultipleModelMixin(object):
         built-in rest_framework filters and custom filters passed into
         the querylist
         """
-        queryset = query_data.get('queryset', [])
-        filterset_class = query_data.get('filterset_class', None)
-
+        queryset = query_data.get("queryset", [])
+        filterset_class = query_data.get("filterset_class", None)
 
         if isinstance(queryset, QuerySet):
             # Ensure queryset is re-evaluated on each request.
             queryset = queryset.all()
-        if self.default_filterset_class is None and self.filterset_class is not None:
-            self.default_filterset_class = filterset_class
 
-        if filterset_class is not None:
-            self.filterset_class = filterset_class
-        else:
-            self.filterset_class = self.default_filterset_class
-
-        # run rest_framework filters
-        queryset = self.filter_queryset(queryset)
+        queryset = self.filter_queryset_custom(queryset, filterset_class)
 
         # run custom filters
-        filter_fn = query_data.get('filter_fn', None)
+        filter_fn = query_data.get("filter_fn", None)
         if filter_fn is not None:
             queryset = filter_fn(queryset, request, *args, **kwargs)
 
@@ -74,6 +66,23 @@ class BaseMultipleModelMixin(object):
 
         return page if page is not None else queryset
 
+    def filter_queryset_custom(self, queryset, filterset_class=None):
+
+        old_filterset_class = getattr(self, "filterset_class", None)
+        for backend in list(self.filter_backends):
+
+            try:
+                from django_filters.rest_framework import DjangoFilterBackend
+
+                if issubclass(backend, DjangoFilterBackend):
+                    self.filterset_class = filterset_class
+            except ImportError:
+                pass
+
+            queryset = backend().filter_queryset(self.request, queryset, self)
+            self.filterset_class = old_filterset_class
+        return queryset
+
     def get_empty_results(self):
         """
         Because the base result type is different depending on the return structure
@@ -81,8 +90,8 @@ class BaseMultipleModelMixin(object):
         `results` variable to the proper type
         """
         assert self.result_type is not None, (
-            '{} must specify a `result_type` value or overwrite the '
-            '`get_empty_result` method.'.format(self.__class__.__name__)
+            "{} must specify a `result_type` value or overwrite the "
+            "`get_empty_result` method.".format(self.__class__.__name__)
         )
 
         return self.result_type()
@@ -93,10 +102,8 @@ class BaseMultipleModelMixin(object):
         data from this queryset/serializer combo
         """
         raise NotImplementedError(
-            '{} must specify how to add data to the running results tally '
-            'by overriding the `add_to_results` method.'.format(
-                self.__class__.__name__
-            )
+            "{} must specify how to add data to the running results tally "
+            "by overriding the `add_to_results` method.".format(self.__class__.__name__)
         )
 
     def format_results(self, results, request):
@@ -118,7 +125,9 @@ class BaseMultipleModelMixin(object):
 
             # Run the paired serializer
             context = self.get_serializer_context()
-            data = query_data['serializer_class'](queryset, many=True, context=context).data
+            data = query_data["serializer_class"](
+                queryset, many=True, context=context
+            ).data
 
             label = self.get_label(queryset, query_data)
 
@@ -165,6 +174,7 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         ...
     ]
     """
+
     # Optional keyword to sort flat lasts by given attribute
     # note that the attribute must by shared by ALL models
     sorting_field = None
@@ -175,14 +185,16 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
     # Django-like model lookups are supported via '__', but you have to be sure that all querysets will return results
     # with corresponding structure.
     sorting_fields_map = {}
-    sorting_parameter_name = 'o'
+    sorting_parameter_name = "o"
 
     # Flag to append the particular django model being used to the data
     add_model_type = True
 
     result_type = list
 
-    _list_attribute_error = 'Invalid sorting field. Corresponding data item is a list: {}'
+    _list_attribute_error = (
+        "Invalid sorting field. Corresponding data item is a list: {}"
+    )
 
     def initial(self, request, *args, **kwargs):
         """
@@ -191,13 +203,15 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         after original `initial` has been ran in order to make sure that view has all its properties set up.
         """
         super(FlatMultipleModelMixin, self).initial(request, *args, **kwargs)
-        assert not (self.sorting_field and self.sorting_fields), \
-            '{} should either define ``sorting_field`` or ``sorting_fields`` property, not both.' \
-            .format(self.__class__.__name__)
+        assert not (
+            self.sorting_field and self.sorting_fields
+        ), "{} should either define ``sorting_field`` or ``sorting_fields`` property, not both.".format(
+            self.__class__.__name__
+        )
         if self.sorting_field:
             warnings.warn(
-                '``sorting_field`` property is pending its deprecation. Use ``sorting_fields`` instead.',
-                DeprecationWarning
+                "``sorting_field`` property is pending its deprecation. Use ``sorting_fields`` instead.",
+                DeprecationWarning,
             )
             self.sorting_fields = [self.sorting_field]
         self._sorting_fields = self.sorting_fields
@@ -207,13 +221,13 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         Gets option label for each datum. Can be used for type identification
         of individual serialized objects
         """
-        if query_data.get('label', False):
-            return query_data['label']
+        if query_data.get("label", False):
+            return query_data["label"]
         elif self.add_model_type:
             try:
                 return queryset.model.__name__
             except AttributeError:
-                return query_data['queryset'].model.__name__
+                return query_data["queryset"].model.__name__
 
     def add_to_results(self, data, label, results):
         """
@@ -222,7 +236,7 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         """
         for datum in data:
             if label is not None:
-                datum.update({'type': label})
+                datum.update({"type": label})
 
             results.append(datum)
 
@@ -236,9 +250,9 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         if self._sorting_fields:
             results = self.sort_results(results)
 
-        if request.accepted_renderer.format == 'html':
+        if request.accepted_renderer.format == "html":
             # Makes the the results available to the template context by transforming to a dict
-            results = {'data': results}
+            results = {"data": results}
 
         return results
 
@@ -249,8 +263,8 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         if not path:
             path = []
         try:
-            if '__' in param:
-                root, new_param = param.split('__')
+            if "__" in param:
+                root, new_param = param.split("__")
                 path.append(root)
                 return self._sort_by(datum[root], param=new_param, path=path)
             else:
@@ -261,9 +275,9 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
                 raise ValidationError(self._list_attribute_error.format(param))
             return data
         except TypeError:
-            raise ValidationError(self._list_attribute_error.format('.'.join(path)))
+            raise ValidationError(self._list_attribute_error.format(".".join(path)))
         except KeyError:
-            raise ValidationError('Invalid sorting field: {}'.format('.'.join(path)))
+            raise ValidationError("Invalid sorting field: {}".format(".".join(path)))
 
     def prepare_sorting_fields(self):
         """
@@ -273,22 +287,26 @@ class FlatMultipleModelMixin(BaseMultipleModelMixin):
         if self.sorting_parameter_name in self.request.query_params:
             # Extract sorting parameter from query string
             self._sorting_fields = [
-                _.strip() for _ in self.request.query_params.get(self.sorting_parameter_name).split(',')
+                _.strip()
+                for _ in self.request.query_params.get(
+                    self.sorting_parameter_name
+                ).split(",")
             ]
 
         if self._sorting_fields:
             # Create a list of sorting parameters. Each parameter is a tuple: (field:str, descending:bool)
             self._sorting_fields = [
-                (self.sorting_fields_map.get(field.lstrip('-'), field.lstrip('-')), field[0] == '-')
+                (
+                    self.sorting_fields_map.get(field.lstrip("-"), field.lstrip("-")),
+                    field[0] == "-",
+                )
                 for field in self._sorting_fields
             ]
 
     def sort_results(self, results):
         for field, descending in reversed(self._sorting_fields):
             results = sorted(
-                results,
-                reverse=descending,
-                key=lambda x: self._sort_by(x, field)
+                results, reverse=descending, key=lambda x: self._sort_by(x, field)
             )
         return results
 
@@ -323,6 +341,7 @@ class ObjectMultipleModelMixin(BaseMultipleModelMixin):
         ...
     }
     """
+
     result_type = dict
 
     def add_to_results(self, data, label, results):
@@ -335,10 +354,10 @@ class ObjectMultipleModelMixin(BaseMultipleModelMixin):
         Gets option label for each datum. Can be used for type identification
         of individual serialized objects
         """
-        if query_data.get('label', False):
-            return query_data['label']
+        if query_data.get("label", False):
+            return query_data["label"]
 
         try:
             return queryset.model.__name__
         except AttributeError:
-            return query_data['queryset'].model.__name__
+            return query_data["queryset"].model.__name__
